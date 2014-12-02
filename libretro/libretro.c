cad90825f4786319180f261a513355665de370b9
Adrien Plazas <kekun.plazas@laposte.net>
2014-12-03 00:10:43 +0100

    libretro: Make load_game() return a valid loading succes state
    This makes retro_load_game() return true if the game have been
    successfuly loaded, and false otherwise, as expected.

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -676,10 +676,10 @@ bool retro_load_game(const struct retro_game_info *info)
    yinit.usethreads = 0;
 #endif
 
-   YabauseInit(&yinit);
+   int result = YabauseInit(&yinit);
    YabauseSetDecilineMode(1);
 
-   return true;
+   return !result;
 }
 
 bool retro_load_game_special(unsigned game_type, const struct retro_game_info *info, size_t num_info)
