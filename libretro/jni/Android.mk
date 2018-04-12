f72f068fcf014d8700c7d231c1aa8924dcf77e1a
Aaron Kling <webgeek1234@gmail.com>
2018-04-12 11:40:29 -0500

    Refactor android jni makefiles

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -1,35 +1,29 @@
 LOCAL_PATH := $(call my-dir)
-DYNAREC := 0
 
-include $(CLEAR_VARS)
+CORE_DIR := $(LOCAL_PATH)/..
+
+FLAGS        :=
+DYNAREC      := 0
+HAVE_THREADS := 1
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+  #DYNAREC := 3
+else ifeq ($(TARGET_ARCH_ABI),x86_64)
+  #DYNAREC := 2
+endif
+
+include $(CORE_DIR)/Makefile.common
+
+COREFLAGS := -DINLINE=inline -D__LIBRETRO__ -DVERSION=\"0.9.12\" -DUSE_SCSP2=1 -DNO_CLI -DHAVE_GETTIMEOFDAY -DHAVE_STRCASECMP $(INCFLAGS) $(FLAGS)
 
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 ifneq ($(GIT_VERSION)," unknown")
-	LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
 
-APP_DIR := ../../src
-
+include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-CORE_DIR := ../
-
-include ../Makefile.common
-
-LOCAL_SRC_FILES    += $(SOURCES_CXX) $(SOURCES_C) $(OBJECTS_S) $(C68KEXEC_SOURCE)
-LOCAL_CFLAGS += -O2 -std=gnu99 -DINLINE=inline -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 -DVERSION=\"0.9.12\" -DUSE_SCSP2=1 -DNO_CLI -DHAVE_GETTIMEOFDAY -I$(SOURCE_DIR) -DHAVE_STRCASECMP $(INCFLAGS)
-
+LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C) $(OBJECTS_S) $(C68KEXEC_SOURCE)
+LOCAL_CFLAGS    := -std=gnu99 $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
 include $(BUILD_SHARED_LIBRARY)
